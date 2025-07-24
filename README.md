33ca0ab16212d4f0d180568d420f00f13a550be4
shenglin <920009579@qq.com>
2025-07-24 14:56:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+#  [ASS](https://github.com/wushuo894/ani-rss) Webhook 通知插件 for [Rana-Bot](https://github.com/sosoorin/RanaBot)
+
+> 支持webhook配置后将ASS订阅的追番消息转发到群
+
+## 配置：
+
+1. 在你的ASS中配置Webhook: 
+- **METHOD**: POST
+- **URL**: `http://你的rana-bot地址:端口/rana-bot/ass/onUpdate`
+- **Body**: onebot消息的JSONArray，以下为示例：
+```
+[
+  {
+    "type": "text",
+    "data": {
+      "text": "番剧【${title}】更新啦！"
+    }
+  },
+  {
+    "type": "image",
+    "data": {
+      "file": "${image}"
+    }
+  },
+  {
+    "type": "text",
+    "data": {
+      "text": "第${season}季 第${episode}/${totalEpisodeNumber}集 - ${bgmJpEpisodeTitle}（${bgmEpisodeTitle}）"
+    }
+  }
+]
+```
+
+2. 在`rana-bot`中启用插件，并配置qq群
+   
+    **todo**: 将QQ群保存到硬盘，下次启动时读取
---
+++ src/main/java/com/sosorin/ranabot/plugin/AssPlugin.java
@@ -0,0 +1,97 @@
+package com.sosorin.ranabot.plugin;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.sosorin.ranabot.ResponseModel;
+import com.sosorin.ranabot.annotation.RanaPlugin;
+import com.sosorin.ranabot.bot.IBot;
+import com.sosorin.ranabot.model.EventBody;
+import com.sosorin.ranabot.model.PluginResult;
+import com.sosorin.ranabot.util.SendEntityUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * @author rana-bot
+ * @since 2025/7/23  16:40
+ */
+@RanaPlugin(value = "assPlugin")
+@RestController
+@RequestMapping("/ass")
+@Slf4j
+public class AssPlugin extends AbstractPlugin {
+
+    private static final Set<String> GROUP_ID_SET = new CopyOnWriteArraySet<>();
+
+    @Autowired
+    private IBot bot;
+
+    public AssPlugin() {
+        super("一个简单的番剧更新通知插件", "0.0.1", "RanaBot");
+    }
+
+    @Override
+    public PluginResult handleEvent(IBot bot, EventBody eventBody) {
+        return PluginResult.CONTINUE();
+    }
+
+    @Override
+    public boolean canHandle(EventBody eventBody) {
+        return false;
+    }
+
+    /**
+     * 番剧更新通知
+     * @param data ass的消息
+     */
+    @PostMapping("/onUpdate")
+    public ResponseModel<?> onUpdate(@RequestBody String data) {
+        if (this.isEnabled()) {
+            JSONArray messageArray = JSON.parseArray(data);
+            log.info("收到番剧更新消息: {}", messageArray);
+            GROUP_ID_SET.forEach(groupId -> {
+                String rawMessage = SendEntityUtil.buildSendGroupMessageStr(groupId, messageArray);
+                log.info("发送群消息: {}", rawMessage);
+                bot.sendRawMessageStr(rawMessage);
+            });
+            return ResponseModel.SUCCESS();
+        }
+        return ResponseModel.FAIL();
+    }
+
+    /**
+     * 设置插件参数
+     *
+     * @param params 插件参数
+     * @return 是否设置成功
+     */
+    @Override
+    public boolean setParams(Map<String, Object> params) {
+        Object set = params.get("groupIdSet");
+        if (set instanceof List<?>) {
+            GROUP_ID_SET.clear();
+            GROUP_ID_SET.addAll((List<String>) set);
+        }
+        return super.setParams(params);
+    }
+
+    /**
+     * 获取插件参数
+     *
+     * @return 插件参数
+     */
+    @Override
+    public Map<String, Object> getParams() {
+        return Map.of("groupIdSet", GROUP_ID_SET);
+    }
+
+}
